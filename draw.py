0bbe398510f4f72d044f2e5f0e5efb78ef58b996
EMilborn <emilborn@stuy.edu>
2017-02-13 11:44:58 -0500

    been there done that

--- draw.py
+++ draw.py
@@ -1,4 +1,18 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    x=x0
+    y=y0
+    A = y1-y0
+    B = -(x1-x0)
+    d = 2*A + B
+
+    while x < x1:
+        plot(x,y)
+
+        if d>0
+            y+=1
+            d += 2*B
+
+        x+=1
+        d+=2*A
