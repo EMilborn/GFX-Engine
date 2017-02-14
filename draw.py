a2809eda404b1ded19558367e2ec101a11dbe28a
EMilborn <emilborn@stuy.edu>
2017-02-14 11:52:40 -0500

    did octant II)

--- draw.py
+++ draw.py
@@ -1,18 +1,40 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
+
+    if (x0 > x1):
+        draw_line(x1, y1, x0, y0, screen, color)
+        return
+        
     x=x0
     y=y0
     A = y1-y0
     B = -(x1-x0)
-    d = 2*A + B
+    d = 2*A + B #(A + 2*B)()()()()(A-2*B)(2*A+B) 
 
-    while x < x1:
-        plot(x,y)
 
-        if d>0
-            y+=1
-            d += 2*B
+    if (A > 0):
+        if(A < -B):#octant I
+    
+            while x < x1:#(y < y1)()()()()(y > y1)(x<x1)
+                plot(screen, color, x, y)
 
-        x+=1
-        d+=2*A
+                if d > 0:#(d < 0)()()()()(d>0)(d < 0) 
+                    y+=1#(x += 1)()()()()(x+=1)(y-=1)
+                    d += 2*B#(d += 2*A)()()()()(d+=2*A)(d-=2*B) 
+
+                x+=1#(y += 1)()()()()(y-=1)(x+=1)
+                d+=2*A#(d += 2B)()()()()(d-=2B)(d+=2A)
+                
+        else:#octant II
+            while y < y1:
+                plot(screen, color, x, y)
+
+                if d < 0: 
+                    x+=1
+                    d += 2*A 
+
+                y+=1
+                d+=2*B
+
+    plot(screen, color, x1, y1)
--- main.py
+++ main.py
@@ -5,4 +5,9 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 
 display(screen)
+
+draw_line(0,0, 23, 61, screen, color)
+
+display(screen)
+
 save_extension(screen, 'img.png')
