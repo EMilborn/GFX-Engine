8b517d8c94405316d89b7487174cfc84796db848
EMilborn <emilborn@stuy.edu>
2017-03-27 11:15:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
 
-from scriptreader import *
+screen = new_screen()
+color = [ 0, 255, 0 ]
+edges = []
+transform = new_matrix()
 
-interpret("script.txt")
+# print_matrix( make_bezier() )
+# print
+# print_matrix( make_hermite() )
+# print
 
+parse_file( 'script', edges, transform, screen, color )
