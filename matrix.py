4802a6a4312ecec7f5aef0409f43fb450bb09959
EMilborn <emilborn@stuy.edu>
2017-03-27 11:17:11 -0400

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,40 +1,109 @@
 import math
-import random
-from draw import *
+
+def make_bezier():
+    return [
+        [-1, 3, -3, 1],
+        [3, -6, 3, 0],
+        [-3, 3, 0, 0],
+        [1, 0, 0, 0]
+    ]
+
+def make_hermite():
+    return [
+        [2, -3, 0, 1],
+        [-2, 3, 0, 0],
+        [1, -2, 1, 0],
+        [1, -1, 0, 0]
+    ]
+
+def generate_curve_coefs( p0, p1, p2, p3, t ):
+    coefs = [[p0, p1, p2, p3]]
+    if t == 'hermite':
+        curve = make_hermite()
+    else:
+        curve = make_bezier()
+
+    matrix_mult(curve, coefs)
+    return coefs
+
+def make_translate( x, y, z ):
+    t = new_matrix()
+    ident(t)
+    t[3][0] = x
+    t[3][1] = y
+    t[3][2] = z
+    return t
+
+def make_scale( x, y, z ):
+    t = new_matrix()
+    ident(t)
+    t[0][0] = x
+    t[1][1] = y
+    t[2][2] = z
+    return t
+
+def make_rotX( theta ):
+    t = new_matrix()
+    ident(t)
+    t[1][1] = math.cos(theta)
+    t[2][1] = -1 * math.sin(theta)
+    t[1][2] = math.sin(theta)
+    t[2][2] = math.cos(theta)
+    return t
+
+def make_rotY( theta ):
+    t = new_matrix()
+    ident(t)
+    t[0][0] = math.cos(theta)
+    t[0][2] = -1 * math.sin(theta)
+    t[2][0] = math.sin(theta)
+    t[2][2] = math.cos(theta)
+    return t
+
+def make_rotZ( theta ):
+    t = new_matrix()
+    ident(t)
+    t[0][0] = math.cos(theta)
+    t[1][0] = -1 * math.sin(theta)
+    t[0][1] = math.sin(theta)
+    t[1][1] = math.cos(theta)
+    return t
 
 def print_matrix( matrix ):
-    for y in range(len(matrix[0])):
-        s=""
-        for x in range(len(matrix)):
-            s+=str(matrix[x][y])
-            s+="\t"
-        print s
-    print
-
+    s = ''
+    for r in range( len( matrix[0] ) ):
+        for c in range( len(matrix) ):
+            s+= str(matrix[c][r]) + ' '
+        s+= '\n'
+    print s
 
 def ident( matrix ):
-    for n in range(len(matrix)):
-        matrix[n][n] = 1
+    for r in range( len( matrix[0] ) ):
+        for c in range( len(matrix) ):
+            if r == c:
+                matrix[c][r] = 1
+            else:
+                matrix[c][r] = 0
 
 def scalar_mult( matrix, s ):
-    for x in range(len(matrix)):
-        for y in range(len(matrix[0])):
-            matrix[x][y] *= s
-
+    for r in range( len( matrix[0] ) ):
+        for c in range( len(matrix) ):
+            matrix[c][r]*= s
+            
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    temp = new_matrix(cols = len(m2))
-    for c in range(len(m2)):
+
+    point = 0
+    for row in m2:
+        #get a copy of the next point
+        tmp = row[:]
+        
         for r in range(4):
-            for n in range (4):
-                temp[c][r] += m2[c][n]*m1[n][r]
-    for c in range(len(m2)):
-        for r in range(4):
-            m2[c][r] = temp[c][r]
-    
-
-
-
+            m2[point][r] = (m1[0][r] * tmp[0] +
+                            m1[1][r] * tmp[1] +
+                            m1[2][r] * tmp[2] +
+                            m1[3][r] * tmp[3])
+        point+= 1
 
 def new_matrix(rows = 4, cols = 4):
     m = []
@@ -43,103 +112,3 @@ def new_matrix(rows = 4, cols = 4):
         for r in range( rows ):
             m[c].append( 0 )
     return m
-
-def scale(dX, dY, dZ, tM):
-    scale = new_matrix()
-    scale[0][0] = dX
-    scale[1][1] = dY
-    scale[2][2] = dZ
-    scale[3][3] = 1
-    matrix_mult(scale, tM)
-
-def translate(dX, dY, dZ, tM):
-    trans = new_matrix()
-    ident(trans)
-    trans[3][0] = dX
-    trans[3][1] = dY
-    trans[3][2] = dZ
-    matrix_mult(trans, tM)
-
-def rotation(axis, deg, tM):
-    rot = new_matrix()
-    ident(rot)
-    angle = deg * math.pi / 180
-    c = math.cos(angle)
-    s = math.sin(angle)
-    if axis == "x":
-        rot[0][0] = c
-        rot[1][1] = c
-        rot[0][1] = -s
-        rot[1][0] = s
-    if axis == "y":
-        rot[1][1] = c
-        rot[2][2] = c
-        rot[1][2] = -s
-        rot[2][1] = s
-    if axis == "z":
-        rot[0][0] = c
-        rot[2][2] = c
-        rot[2][0] = -s
-        rot[0][2] = s
-    matrix_mult(rot, tM)
-
-def circle(edges, cx, cy, cz, r):
-    print "new circ"
-    s = 100
-    x = cx + r
-    y = cy
-    for t in range(1,s+1):
-        add_point(edges, x, y, cz)
-        x = math.cos(math.pi*2*t/s)
-        x*=r
-        x+=cx
-        y = math.sin(math.pi*2*t/s)
-        y*=r
-        y+=cy
-        add_point(edges, x, y, cz)
-    
-
-def hermite(edges, x0, y0, x1, y1, rx0, ry0, rx1, ry1):
-    s = 100.0
-    mult = [[2, -3,  0,  1],
-            [-2,  3,  0,  0],
-            [ 1, -2,  1,  0],
-            [ 1, -1,  0,  0]
-            ]
-    fx = [[x0, x1, rx0,rx1]]
-    fy = [[y0, y1, ry0, ry1]]
-    matrix_mult(mult, fx)
-    matrix_mult(mult, fy)
-    fx = fx[0]
-    fy = fy[0] 
-    x=x0
-    y=y0
-    for t in range(1,int(s)):
-        add_point(edges, x, y, 0)
-        x = fx[0]*(t/s)**3 + fx[1]*(t/s)**2 + fx[2]*(t/s) + fx[3]
-        y = fy[0]*(t/s)**3 + fy[1]*(t/s)**2 + fy[2]*(t/s) + fy[3]
-        add_point(edges, x, y, 0)
-    
-
-def bezier(edges, x0, y0, x1, y1, x2, y2, x3, y3):
-    s = 100.0
-    mult = [[-1,  3, -3,  1],
-            [ 3, -6,  3,  0],
-            [-3,  3,  0,  0],
-            [ 1,  0,  0,  0]
-            ]
-    fx = [[x0, x1, x2, x3]]
-    fy = [[y0, y1, y2, y3]]
-    matrix_mult(mult, fx)
-    matrix_mult(mult, fy)
-    fx = fx[0]
-    fy = fy[0] 
-    x = x0
-    y = y0
-    for t in range(1,int(s)+1):
-        add_point(edges, x, y, 0)
-        x = fx[0]*(t/s)**3 + fx[1]*(t/s)**2 + fx[2]*(t/s) + fx[3]
-        y = fy[0]*(t/s)**3 + fy[1]*(t/s)**2 + fy[2]*(t/s) + fy[3]
-        
-        add_point(edges, x, y, 0)
-    pass
