8ed02664266f18775683e7f4910ba74bb406c468
EMilborn <emilborn@stuy.edu>
2017-05-03 11:48:13 -0400

    doesnt work

--- script.py
+++ script.py
@@ -20,9 +20,117 @@ def run(filename):
         return
 
     ident(tmp)
-    stack = [ [x[:] for x in tmp] ]
+    transform = [ [x[:] for x in tmp] ]
     screen = new_screen()
     tmp = []
     step = 0.1
     for command in commands:
-        print command
+        line=command[0]
+        try:
+            args=command[1:]
+        except:
+            pass
+        if line == 'push':
+            transform.append(transform[len(transform)-1][:])
+            
+        if line == 'pop':
+            transform = transform[:-1]
+
+        if line == 'circle':
+            #print 'CIRCLE\t' + str(args)
+            temp = []
+            add_circle(temp,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
+            matrix_mult(transform[-1],temp)
+            draw_lines(temp, screen, color)
+
+        elif line == 'hermite' or line == 'bezier':
+            #print 'curve\t' + line + ": " + str(args)
+            temp = []
+            add_curve(edges,
+                      float(args[0]), float(args[1]),
+                      float(args[2]), float(args[3]),
+                      float(args[4]), float(args[5]),
+                      float(args[6]), float(args[7]),
+                      step, line)
+            matrix_mult(transform[-1],temp)
+            draw_lines(temp, screen, color) 
+            
+        elif line == 'line':            
+            #print 'LINE\t' + str(args)
+            temp=[]
+            add_edge( temp,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult(transform[-1],temp)
+
+        elif line == 'scale':
+            #print 'SCALE\t' + str(args)
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(transform[-1],t)
+            transform[-1]=t
+
+        elif line == 'move':
+            #print 'MOVE\t' + str(args)
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(transform[-1],t)
+            transform[-1]=t
+
+        elif line == 'rotate':
+            #print 'ROTATE\t' + str(args)
+            theta = float(args[1]) * (math.pi / 180)
+            
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult(transform[-1],t)
+            transform[-1]=t
+
+        elif line == 'box':
+            for i in range(len(args)):
+                args[i] = int(args[i])
+            temp = []
+            add_box(temp, args[0], args[1], args[2], args[3], args[4], args[5])
+            print len(temp)
+            matrix_mult(transform[-1],temp)
+            draw_polygons(temp, screen, color)
+                
+        elif line == 'torus':
+            for i in range(len(args)):
+                args[i] = int(args[i])
+            temp = []
+            add_torus(temp, args[0], args[1], args[2], args[3], args[4],.1)
+            matrix_mult(transform[-1],temp)
+            draw_polygons(temp, screen, color)
+
+        elif line == 'sphere':
+            for i in range(len(args)):
+                args[i] = int(args[i])
+            temp = []
+            add_sphere(temp, args[0], args[1], args[2], args[3], .04999999)
+            matrix_mult(transform[-1],temp)
+            draw_polygons(temp, screen, color)
+
+            
+        elif line == 'ident':
+            ident(transform)
+
+        elif line == 'apply':
+            matrix_mult( transform, edges )
+
+        elif line == 'display' or line == 'save':
+            if line == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
+
+        elif line == 'clear':
+            edges = [[]]
+            
+                
+        
+run('script.mdl')
