424aabfd28c01fe23a80a550661b7662e50851eb
EMilborn <emilborn@stuy.edu>
2017-05-11 11:27:00 -0400

    Update mdl.py

--- mdl.py
+++ mdl.py
@@ -1,4 +1,3 @@
-
 import lex, yacc
 
 tokens = (
@@ -126,7 +125,7 @@ lex.lex()
 #----------------------------------------------------------
 
 commands = []
-symbols = []
+symbols = {}
 
 def p_stuff(p):
     """stuff : 
@@ -136,6 +135,19 @@ def p_stuff(p):
 def p_statement_comment(p):
     'statement : COMMENT'
     pass
+
+def p_statement_frames(p):
+    """statement : FRAMES INT"""
+    commands.append(p[1:])
+
+def p_statement_basename(p):
+    """statement : BASENAME TEXT"""
+    commands.append(p[1:])
+
+def p_statement_vary(p):
+    """statement : VARY SYMBOL INT INT NUMBER NUMBER"""
+    commands.append(p[1:])
+    symbols[p[2]] = ['knob', 0]
 
 def p_statement_stack(p):
     """statement : POP
@@ -168,8 +180,9 @@ def p_statement_knobs(p):
                  | SET_KNOBS NUMBER"""
     commands.append(tuple(p[1:]))
     if p[1] == "set":
-        symbols.append(("knob", p[2]))
-
+        #symbols.append(("knob", p[2]))
+        symbols[p[2]] = ['knob', 0]
+        
 def p_statement_sphere(p):
     """statement : SPHERE NUMBER NUMBER NUMBER NUMBER INT INT
                  | SPHERE NUMBER NUMBER NUMBER NUMBER"""
@@ -222,8 +235,8 @@ def p_statement_move(p):
         commands.append(tuple(p[1:] + [None]))
     else:
         commands.append(tuple(p[1:]))
-        symbols.append(("knob", p[5]))
-
+        symbols[p[5]] = ['knob', 0]
+        
 def p_statement_scale(p):
     """statement : SCALE NUMBER NUMBER NUMBER SYMBOL
                  | SCALE NUMBER NUMBER NUMBER"""
@@ -231,8 +244,8 @@ def p_statement_scale(p):
         commands.append(tuple(p[1:] + [None]))
     else:
         commands.append(tuple(p[1:]))
-        symbols.append(("knob", p[5]))
-
+        symbols[p[5]] = ['knob', 0]
+        
 def p_statement_rotate(p):
     """statement : ROTATE XYZ NUMBER SYMBOL
                  | ROTATE XYZ NUMBER"""
@@ -240,8 +253,8 @@ def p_statement_rotate(p):
         commands.append(tuple(p[1:] + [None]))
     else:
         commands.append(tuple(p[1:]))
-        symbols.append(("knob", p[4]))
-
+        symbols[p[4]] = ['knob', 0]
+        
 def p_SYMBOL(p):
     """SYMBOL : XYZ
               | ID"""
@@ -259,6 +272,8 @@ def p_NUMBER(p):
 
 yacc.yacc()
 
+from copy import deepcopy
+
 def parseFile(filename):
     """
     This funstion returns a tuple containing a list of opcodes
@@ -270,16 +285,16 @@ def parseFile(filename):
     global commands
     global symbols
     commands = []
-    symbols = []
+    symbols = {}
     try:
         f = open(filename, "r")
         for line in f.readlines():
             line = line.strip()
             yacc.parse(line)
         f.close()
-        result = (commands[:], symbols[:])
+        result = (commands[:], deepcopy(symbols))
         commands = []
-        symbols = []
+        symbols = {}
         return result
     except IOError:
         return ()
